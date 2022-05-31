3d9faf661f0efb5584801ab0b8ed52f60dda9764
Raul <raulfernan000@gmail.com>
2022-05-31 11:45:49 +0200

    practice done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'asdf';
+  asdfs = [
+    'asdf',
+    'sdfa',
+    'dfas',
+    'fasd',
+    'fdsa',
+    'dsaf',
+    'safd',
+    'afds',
+    'etc...',
+  ];
+  newAsdf = "";
+  selection = "asdf";
+
+  changeTitle() {
+    this.title = 'myapp';
+  }
+  listAsdfs() {
+    return this.asdfs.length;
+  }
+  addNewAsdf() {
+    this.asdfs.push(this.newAsdf);
+    this.newAsdf = "";
+  }
 }
